ac2e110cc20d8f74eda09e65afadf9657436d906
Deepak Bhadoriya <48173231+deepakbhadoriya@users.noreply.github.com>
2020-01-18 15:03:35 +0530

    Update database.js

--- setup/database.js
+++ setup/database.js
@@ -1,5 +1,6 @@
 module.exports = {
   mongoURL:
-    "mongodb+srv://deepak:deepak@cluster0-cza3r.mongodb.net/test?retryWrites=true&w=majority",
+    //add your monog database here
+  ,
   secret: "mystrongsecret"
 };
